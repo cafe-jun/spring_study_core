bba7fa6fbce47b9e0df54c0d03d82155651443af
tb25271@gmail.com <tb25271@gmail.com>
2022-10-26 20:07:18 +0900

    [update] 주문 할인정책 개발

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,44 @@
+package hello.core.order;
+
+public class Order {
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int disCountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int disCountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.disCountPrice = disCountPrice;
+    }
+    public int calculatePrice() {
+        return itemPrice - disCountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDisCountPrice() {
+        return disCountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", disCountPrice=" + disCountPrice +
+                '}';
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        // when
+        Member member = new Member(memberId,"itemA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId,"itemA",10000);
+        // then
+        Assertions.assertThat(order.getDisCountPrice()).isEqualTo(1000);
+    }
+}
