79adc01f933f5f10aa8ed04ddcb658eae92d8980
tb25271@gmail.com <tb25271@gmail.com>
2022-10-27 20:20:23 +0900

    [update] OCP,DIP 이상

---
+++ src/test/java/hello/core/discount/RatePolicyDiscountTest.java
@@ -0,0 +1,35 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RatePolicyDiscountTest {
+
+    RatePolicyDiscount  ratePolicyDiscount = new RatePolicyDiscount();
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용이 되어야한다")
+    void vip_o() {
+        // given
+        Member member = new Member(1L,"memberVIP", Grade.VIP);
+        // when
+        int discount = ratePolicyDiscount.discount(member,10000);
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+    @Test
+    @DisplayName("VIP아닌 유저는 10% 할인이 제외 되어야한다")
+    void vip_x() {
+        // given
+        Member member = new Member(1L,"memberVIP", Grade.BASIC);
+        // when
+        int discount = ratePolicyDiscount.discount(member,10000);
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+
+}
